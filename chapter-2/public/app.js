01a41372dd89e72c568ab3bf1dc3555788012389
sugeng20 <sugeng.wanantara@gmail.com>
2023-07-20 11:03:43 +0700

    Setup Babel Dan Dependencies Lainnya

---
+++ chapter-2/public/app.js
@@ -0,0 +1,9 @@
+const root = document.querySelector("#root");
+function tick() {
+  const element = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Jam Sekarang :"), /*#__PURE__*/React.createElement("p", null, new Date().toLocaleTimeString())));
+  ReactDOM.render(element, root);
+}
+tick();
+setInterval(() => {
+  tick();
+}, 1000);
---
+++ chapter-2/src/app.js
@@ -0,0 +1,19 @@
+const root = document.querySelector("#root");
+
+function tick() {
+  const element = (
+    <>
+      <div>
+        <h1>Jam Sekarang :</h1>
+        <p>{new Date().toLocaleTimeString()}</p>
+      </div>
+    </>
+  );
+  ReactDOM.render(element, root);
+}
+
+tick();
+
+setInterval(() => {
+  tick();
+}, 1000);
